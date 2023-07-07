d3e8a9bc45ef89dcb9be0c57a617a5d2ec517707
kikiyoshi <91988654+kikiyoshi@users.noreply.github.com>
2023-07-07 19:03:43 -0300

    Add files via upload

--- arm_buffer_rack/arduino/move_buffer_rack/move_buffer_rack.ino
+++ arm_buffer_rack/arduino/move_buffer_rack/move_buffer_rack.ino
@@ -23,6 +23,291 @@ int buff = 0;
 int rack = 0;
 int mov = 0;
 int pos = 0;
+char b[] = "buffer";
+char r[] = "rack";
+char m[] = "movimentando";
+
+void setup()
+{
+  Serial.begin(9600);
+  Serial.print("iniciando....");
+  rpServo1.attach(pinoServo1);
+  rpServo2.attach(pinoServo2);
+  rpServo3.attach(pinoServo3);
+  rpServo4.attach(pinoServo4);
+  pinMode(botao1, INPUT);
+  attachInterrupt(digitalPinToInterrupt(botao1),capture1, RISING);
+  pinMode(botao2, INPUT);
+  attachInterrupt(digitalPinToInterrupt(botao2),capture2, RISING);
+  pinMode(botao3, INPUT);
+  attachInterrupt(digitalPinToInterrupt(botao3),capture3, RISING);
+  pinMode(botao4, INPUT);
+  attachInterrupt(digitalPinToInterrupt(botao4),capture4, RISING);
+  rpServo1.write(110);
+  rpServo2.write(30);
+  rpServo3.write(180);
+  rpServo4.write(125);
+
+  delay(10000); //wait 30 seconds (to set up the multi-agent system)
+}
+
+void capture1(){
+ buff = 0;
+ att = 1;
+}
+
+void capture2(){
+ buff = 1;
+ att = 1;
+}
+
+void capture3(){
+ rack = 0;
+ att = 1;
+}
+
+void capture4(){
+ rack = 1;
+ att = 1;
+}
+
+
+void loop()
+{ 
+  while(Serial.available() > 0){
+     String s = Serial.readString();
+     if(s.equals("captBuffer")){
+      att = 1;
+      for(pos = 110; pos >= 5; pos -= 1){
+        rpServo1.write(pos);
+        delay(15);
+      }
+      for(pos = 30; pos <= 120; pos += 1){
+        rpServo2.write(pos);
+        delay(15);
+      }
+      for(pos = 180; pos >= 115; pos -= 1){
+        rpServo3.write(pos);
+        delay(15);
+      }
+      for(pos = 125; pos <= 170; pos += 1){
+        rpServo4.write(pos);
+        delay(15);
+      }
+      for(pos = 115; pos <= 180; pos += 1){
+        rpServo3.write(pos);
+        delay(15);
+      }
+      for(pos = 120; pos >= 30; pos -= 1){
+        rpServo2.write(pos);
+        delay(15);
+      }
+      for(pos = 5; pos <= 175; pos += 1){
+        rpServo1.write(pos);
+        delay(15);
+      }
+      for(pos = 30; pos <= 120; pos += 1){
+        rpServo2.write(pos);
+        delay(15);
+      }
+      for(pos = 180; pos >= 115; pos -= 1){
+        rpServo3.write(pos);
+        delay(15);
+      }
+      for(pos = 170; pos >= 125; pos -= 1){
+        rpServo4.write(pos);
+        delay(15);
+      }
+      rack = 1;
+
+      for(pos = 115; pos <= 180; pos += 1){
+        rpServo3.write(pos);
+        delay(15);
+      }
+      for(pos = 120; pos >= 30; pos -= 1){
+        rpServo2.write(pos);
+        delay(15);
+      }
+      for(pos = 175; pos >= 110; pos -= 1){
+        rpServo1.write(pos);
+        delay(15);
+      }
+      buff = 0;
+      mov = 0;
+    }
+
+    if(s.equals("captRack")){
+      for(pos = 110; pos <= 175; pos += 1){
+        rpServo1.write(pos);
+        delay(15);
+      }
+      for(pos = 30; pos <= 120; pos += 1){
+        rpServo2.write(pos);
+        delay(15);
+      }
+      for(pos = 180; pos >= 115; pos -= 1){
+        rpServo3.write(pos);
+        delay(15);
+      }
+      for(pos = 125; pos <= 170; pos += 1){
+        rpServo4.write(pos);
+        delay(15);
+      }
+      for(pos = 115; pos <= 180; pos += 1){
+        rpServo3.write(pos);
+        delay(15);
+      }
+      rack = 0;
+      
+      for(pos = 120; pos >= 30; pos -= 1){
+        rpServo2.write(pos);
+        delay(15);
+      }
+      for(pos = 175; pos >= 110; pos -= 1){
+        rpServo1.write(pos);
+        delay(15);
+      }
+      for(pos = 30; pos <= 120; pos += 1){
+        rpServo2.write(pos);
+        delay(15);
+      }
+      for(pos = 180; pos >= 115; pos -= 1){
+        rpServo3.write(pos);
+        delay(15);
+      }
+      for(pos = 170; pos >= 125; pos -= 1){
+        rpServo4.write(pos);
+        delay(15);
+      }
+      for(pos = 115; pos <= 180; pos += 1){
+        rpServo3.write(pos);
+        delay(15);
+      }
+      for(pos = 120; pos >= 30; pos -= 1){
+        rpServo2.write(pos);
+        delay(15);
+      }
+    }
+
+    if(s.equals("captCar")){
+      att = 1;
+      for(pos = 30; pos <= 120; pos += 1){
+        rpServo2.write(pos);
+        delay(15);
+      }
+      for(pos = 180; pos >= 115; pos -= 1){
+        rpServo3.write(pos);
+        delay(15);
+      }
+      for(pos = 125; pos <= 170; pos += 1){
+        rpServo4.write(pos);
+        delay(15);
+      }
+      for(pos = 115; pos <= 180; pos += 1){
+        rpServo3.write(pos);
+        delay(15);
+      }
+      for(pos = 120; pos >= 30; pos -= 1){
+        rpServo2.write(pos);
+        delay(15);
+      }
+      for(pos = 110; pos <= 175; pos += 1){
+        rpServo1.write(pos);
+        delay(15);
+      }
+      for(pos = 30; pos <= 120; pos += 1){
+        rpServo2.write(pos);
+        delay(15);
+      }
+      for(pos = 180; pos >= 115; pos -= 1){
+        rpServo3.write(pos);
+        delay(15);
+      }
+      for(pos = 170; pos >= 125; pos -= 1){
+        rpServo4.write(pos);
+        delay(15);
+      }
+      rack = 1;
+
+      for(pos = 115; pos <= 180; pos += 1){
+        rpServo3.write(pos);
+        delay(15);
+      }
+      for(pos = 120; pos >= 30; pos -= 1){
+        rpServo2.write(pos);
+        delay(15);
+      }
+      for(pos = 175; pos >= 110; pos -= 1){
+        rpServo1.write(pos);
+        delay(15);
+      }
+      mov = 0;
+     }
+     
+     if(s.equals("movArm")){
+      att = 1;
+      mov = 1;
+     }
+   }
+
+  if(att == 1){
+    if(buff == 0){
+      communication.startBelief("emptyB");
+    }
+    else{
+      communication.startBelief("fullB");
+    }
+    communication.beliefAdd(b);
+    communication.endBelief();
+    if(rack == 0){
+      communication.startBelief("emptyR");
+    }
+    else{
+      communication.startBelief("fullR");
+    }
+    communication.beliefAdd(r);
+    communication.endBelief();
+    if(mov == 0){
+      communication.startBelief("stop");
+    }
+    else{
+      communication.startBelief("play");
+    }
+    communication.beliefAdd(m);
+    communication.endBelief();
+    communication.sendMessage();
+    att = 0;
+  }
+}
+
+
+
+/*
+#include<Embedded_Protocol_2.h>
+#include<ArduinoJson.h>
+#include <Servo.h>
+
+Communication communication;
+
+Servo rpServo1;
+Servo rpServo2;
+Servo rpServo3;
+Servo rpServo4;
+
+#define botao1 0
+#define botao2 1
+#define botao3 2
+#define botao4 3
+#define pinoServo1 10
+#define pinoServo2 11
+#define pinoServo3 12
+#define pinoServo4 13
+
+int att = 1;
+int buff = 0;
+int rack = 0;
+int mov = 0;
+int pos = 0;
 int b = 0;
 int r = 0;
 
@@ -263,3 +548,4 @@ void loop()
     att = 0;
   }
 }
+*/
--- arm_buffer_rack/src/agt/sample_agent.asl
+++ arm_buffer_rack/src/agt/sample_agent.asl
@@ -1,6 +1,65 @@
-
 !start.
-!emptyBuffer.
+!captyBuffer. //Initial goal
+
++!start : true <- .print("Hello world.").
+
++fullR("rack")
+  <- .print("Rack detected full.").
+
++emptyR("rack")
+  <- .print("Rack detected empty.").
+
++!captyBuffer : play("movimentando")
+ <- .wait(stop("movimentando"));
+    !captyBuffer.
+
++!captyBuffer : fullB("buffer")
+  <- !move_buffer;
+     !captyBuffer.
+
++!captyBuffer : emptyB("buffer")
+  <- .print("Esperando buffer.");
+     .wait(fullB("buffer"));
+     !captyBuffer.
+
++!captyBuffer
+  <- .wait(fullB("buffer"));
+     !captyBuffer.
+
+
++!move_buffer : emptyR("rack")
+  <- embedded.mas.bridges.jacamo.defaultEmbeddedInternalAction("arduino1","mov_A",[]);
+     .print("Buffer detected. Rack is empty.");
+     embedded.mas.bridges.jacamo.defaultEmbeddedInternalAction("arduino1","capt_B",[]).
++!move_buffer : fullR("rack")
+  <- embedded.mas.bridges.jacamo.defaultEmbeddedInternalAction("arduino1","mov_A",[]);
+     .print("Buffer detected. Rack is full.");
+     embedded.mas.bridges.jacamo.defaultEmbeddedInternalAction("arduino1","capt_R",[]);
+     .wait(10000);
+     .send(delivery,achieve,move_delivery);
+     embedded.mas.bridges.jacamo.defaultEmbeddedInternalAction("arduino1","capt_B",[]);
+     .wait(stop("movimentando")).
++!move_buffer
+  <- .print("Buffer: Unidentified Rack.");
+     +emptyB("buffer").
+
++!remove_delivery[source(Sender)] : emptyR("rack")
+  <- embedded.mas.bridges.jacamo.defaultEmbeddedInternalAction("arduino1","mov_A",[]);
+     .print("Delivery detected. Rack is empty.");
+     embedded.mas.bridges.jacamo.defaultEmbeddedInternalAction("arduino1","capt_C",[]);
+     .wait(3000);
+     .send(delivery,achieve,move_delivery);
+     .wait(stop("movimentando")).
++!remove_delivery[source(Sender)] : fullR("rack")
+  <- .print("Delivery detected. Rack is full, not possible.").
++!remove_delivery[source(Sender)]
+   <- .print("Delivery: Unidentified Rack.").
+
+
+
+/*
+!start.
+!captyBuffer. //Initial goal
 
 +!start : true <- .print("Hello world.").
 
@@ -10,22 +69,22 @@
 +rack(0)
   <- .print("Rack detected empty.").
 
-+!emptyBuffer : movimentando(1)
++!captyBuffer : movimentando(1)
  <- .wait(movimentando(0));
-    !emptyBuffer.
+    !captyBuffer.
 
-+!emptyBuffer : buffer(1)
++!captyBuffer : buffer(1)
   <- !move_buffer;
-     !emptyBuffer.
+     !captyBuffer.
 
-+!emptyBuffer : buffer(0)
++!captyBuffer : buffer(0)
   <- .print("Esperando buffer.");
      .wait(buffer(1));
-     !emptyBuffer.
+     !captyBuffer.
 
-+!emptyBuffer
++!captyBuffer
   <- .wait(buffer(1));
-     !emptyBuffer.
+     !captyBuffer.
 
 
 +!move_buffer : rack(0)
@@ -55,122 +114,4 @@
   <- .print("Delivery detected. Rack is full, not possible.").
 +!remove_delivery[source(Sender)]
    <- .print("Delivery: Unidentified Rack.").
-
-
-
-
-
-
-
-/*
-   !start.
-   !emptyBuffer.
-
-   +!start : true <- .print("Hello world.").
-
-   +rack(1)
-     <- .print("Rack detected full.").
-
-   +rack(0)
-     <- .print("Rack detected empty.").
-
-   +movimentando(Atual) : movimentando(Anterior) & Atual\==Anterior
-     <- -movimentando(Anterior).
-
-   +!emptyBuffer : movimentando(1)
-    <- .wait(movimentando(0));
-       !emptyBuffer.
-
-   +!emptyBuffer : buffer(1)
-     <- !move_buffer;
-        !emptyBuffer.
-
-   +!emptyBuffer : buffer(0)
-     <- .print("Esperando buffer.");
-        .wait(buffer(1));
-        !emptyBuffer.
-
-   +!emptyBuffer
-     <- .wait(buffer(1));
-        !emptyBuffer.
-
-
-   +!move_buffer : rack(0)
-     <- +movimentando(1);
-        .print("Buffer detected. Rack is empty.");
-        embedded.mas.bridges.jacamo.defaultEmbeddedInternalAction("arduino1","capt_B",[]).
-   +!move_buffer : rack(1)
-     <- +movimentando(1);
-        .print("Buffer detected. Rack is full.");
-        embedded.mas.bridges.jacamo.defaultEmbeddedInternalAction("arduino1","capt_R",[]);
-        .wait(10000);
-        .send(delivery,achieve,move_delivery);
-        embedded.mas.bridges.jacamo.defaultEmbeddedInternalAction("arduino1","capt_B",[]);
-        .wait(movimentando(0)).
-   +!move_buffer
-     <- .print("Buffer: Unidentified Rack.");
-        +buffer(0).
-
-   +!remove_delivery[source(Sender)] : rack(0)
-     <- +movimentando(1);
-        .print("Delivery detected. Rack is empty.");
-        embedded.mas.bridges.jacamo.defaultEmbeddedInternalAction("arduino1","capt_C",[]);
-        .wait(3000);
-        .send(delivery,achieve,move_delivery);
-        .wait(movimentando(0)).
-   +!remove_delivery[source(Sender)] : rack(1)
-     <- .print("Delivery detected. Rack is full, not possible.").
-   +!remove_delivery[source(Sender)]
-      <- .print("Delivery: Unidentified Rack.").
-*/
-
-
-
-
-
-
-
-
-
-/*
-   !start.
-
-   +!start : true <- .print("Hello world.").
-
-   +buffer(1)
-     <- !move_buffer.
-
-   +rack(1)
-     <- -rackEmpty;
-        .print("Rack detected full.");
-        +rackFull.
-
-   +rack(0)
-     <- -rackFull;
-        .print("Rack detected empty.");
-        +rackEmpty.
-
-
-   +!move_buffer : rack(0)
-     <- .print("Buffer detected. Rack is empty.");
-        embedded.mas.bridges.jacamo.defaultEmbeddedInternalAction("arduino1","capt_B",[]).
-   +!move_buffer : rack(1)
-     <- .print("Buffer detected. Rack is full.");
-        embedded.mas.bridges.jacamo.defaultEmbeddedInternalAction("arduino1","capt_R",[]);
-        .wait(10000);
-        .send(delivery,achieve,move_delivery);
-        embedded.mas.bridges.jacamo.defaultEmbeddedInternalAction("arduino1","capt_B",[]).
-   +!move_buffer
-     <- .print("Buffer: Unidentified Rack.");
-        +buffer(0).
-
-   +!remove_delivery[source(Sender)] : rack(0)
-     <- .print("Delivery detected. Rack is empty.");
-        embedded.mas.bridges.jacamo.defaultEmbeddedInternalAction("arduino1","capt_C",[]);
-        .wait(3000);
-        .send(delivery,achieve,move_delivery).
-   +!remove_delivery[source(Sender)] : rack(1)
-     <- .print("Delivery detected. Rack is full, not possible.").
-   +!remove_delivery[source(Sender)]
-      <- .print("Delivery: Unidentified Rack.").
 */
